2324036ee877cb40cf3719e96c72a205523d526a
voce <voce@localhost.localdomain>
2017-11-04 07:17:00 -0200

    DAOs e comandos Hibernate... falta testar

--- src/cmd/DAO/ItemDAO.java
+++ src/cmd/DAO/ItemDAO.java
@@ -1,14 +1,89 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//TODO: Testar
 package cmd.DAO;
 
+import cmd.entidade.Item;
+import cmd.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
 /**
- *
- * @author voce
+ * Classe de DAO para Item (derivado de Construção; referente a Orçamento)
+ * @author ian-melo
  */
-public class ItemDAO {
+public class ItemDAO implements DAO<Item> {
+
+    @Override
+    public boolean inserir(Item item) {
+        try {
+            Session s = HibernateUtil.getSessionFactory().openSession();
+            s.beginTransaction();
+            s.save(item);
+            s.getTransaction().commit();
+            s.close();
+            return true;
+        } catch(HibernateException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean alterar(Item item) {
+        try {
+            Session s = HibernateUtil.getSessionFactory().openSession();
+            s.beginTransaction();
+            s.update(item);
+            s.getTransaction().commit();
+            s.close();
+            return true;
+        } catch(HibernateException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean excluir(Item item) {
+        try {
+            Session s = HibernateUtil.getSessionFactory().openSession();
+            s.beginTransaction();
+            s.delete(item);
+            s.getTransaction().commit();
+            s.close();
+            return true;
+        } catch(HibernateException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public Item buscar(String consulta) {
+        try {
+            Item i;
+            Session s = HibernateUtil.getSessionFactory().openSession();
+            s.beginTransaction();
+            i = (Item) (s.createQuery("from Item where CodItem = :cod")
+                .setInteger("cod", Integer.parseInt(consulta)).list().get(0));
+            s.getTransaction().commit();
+            s.close();
+            return i;
+        } catch(HibernateException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public List<Item> listar() {
+        try {
+            List li;
+            Session s = HibernateUtil.getSessionFactory().openSession();
+            s.beginTransaction();
+            li = s.createQuery("from Item").list();
+            s.getTransaction().commit();
+            s.close();
+            return li;
+        } catch(HibernateException e) {
+            return null;
+        }
+    }
     
 }
